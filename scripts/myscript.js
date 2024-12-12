0d1379459dba90758a81cc847a0b3fb4367ca5f8
erinma6 <erinma6@gmail.com>
2024-12-12 17:01:00 -0500

    Rendered

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -1,1 +1,8 @@
 // add your JavaScript/D3 to this file
+d3.csv("../result.csv")
+    .then(function(data) {
+        console.log('data loaded');
+    })
+    .catch(function(error) {
+        console.error("Error loading CSV:", error);  // Log any errors related to CSV loading
+    });
